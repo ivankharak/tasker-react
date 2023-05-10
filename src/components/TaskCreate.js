2007017d8e68c6277b77cef878114fc65f3041ab
ivankharak <ivankharak@gmail.com>
2023-05-10 18:27:40 +0530

    v1 Delta 1.5 > > initialized the state 'title' to keep up with the title that user enters

--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 
 const TaskCreate = ({ onCreate }) => {
+    const [title, setTitle] = useState('');
 
     return (
         <div>
             <h3>Create a Task</h3>
             <label>Title</label>
             <br />
-            <input placeholder="Type your task" />
+            <input />
         </div>
     )
 }
